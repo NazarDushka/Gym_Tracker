3e00c781786e902968554ab2d5d8428952dfa686
NazarDushka <dushkanazar@gmail.com>
2026-06-05 00:09:07 +0200

    hot fix

--- Gym-Tracker-Web/src/environments/environment.development.ts
+++ Gym-Tracker-Web/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-  apiUrl: 'http://192.168.1.70:7079',
+    production: true, 
+  apiUrl: 'https://gymtracker-app-20260531234016.ambitiousdune-b9879ff6.polandcentral.azurecontainerapps.io'
 };
